b676deb9863ee803cdb32a539d1e2f54d19d211d
Kaique Savi <121976846+KaiqueSavi@users.noreply.github.com>
2025-04-29 08:26:39 -0300

    testes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-
-import tailwindcss from '@tailwindcss/vite';
+import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), tailwind()],
+  adapter: cloudflare(),
+  output: 'server',
   vite: {
     ssr: {
       noExternal: ['@radix-ui/react-accordion']
     },
-
     resolve: {
       alias: {
         '@': process.env.NODE_ENV === 'production' ? '/src' : '/src'
       }
-    },
-
-    plugins: [tailwindcss()]
+    }
   }
 });
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#ff7900',
+        secondary: '#001256'
+      }
+    },
+  },
+  plugins: [],
+}
+
