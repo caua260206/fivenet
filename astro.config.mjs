1fa7539179f7451bf7513489c7a78ef74ad9bb74
Kaique Savi <121976846+KaiqueSavi@users.noreply.github.com>
2025-04-29 08:10:05 -0300

    teste

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind'; // CORRETO
+import tailwindcss from "@tailwindcss/vite";
 import react from '@astrojs/react';
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    tailwind(), // Use o plugin oficial do Astro para Tailwind
     react()
   ],
   adapter: cloudflare(),
@@ -16,6 +15,7 @@ export default defineConfig({
   },
   output: 'server',
   vite: {
+    plugins: [tailwindcss()],
     resolve: {
       alias: import.meta.env.PROD && {
         "react-dom/server": "react-dom/server.edge",
