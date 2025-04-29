ec88472f828ed635d3ff5b670f7c3bbc6ebe4704
Kaique Savi <121976846+KaiqueSavi@users.noreply.github.com>
2025-04-29 08:03:36 -0300

    teste

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+// @ts-ignore
 import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
@@ -9,6 +10,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  // @ts-ignore
   vite: {
     plugins: [tailwindcss()]
   },
@@ -18,5 +20,15 @@ export default defineConfig({
   experimental: {
     session: true, // Ativa a funcionalidade de sessão
   },
-  output: 'server'
+  output: 'server',
+  vite: {
+    resolve: {
+      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+      // @ts-ignore
+      alias: import.meta.env.PROD && {
+        "react-dom/server": "react-dom/server.edge",
+      },
+    },
+// @ts-ignore
 });
